8265c9a63af2ec5d2fc2fd305c0b54fcd59afb4e
Antonio <antonpogoryelov@gmail.com>
2024-07-13 14:00:26 +0300

    Main code
    All classes, methods & comments

---
+++ OB03_Zoo.py
@@ -0,0 +1,92 @@
+#Создаём базовый класс животных
+class Animal:
+    def __init__(self, name, sound, age):
+        self.name = name
+        self.sound = sound
+        self.age = age
+
+    def make_sound(self):
+        pass
+
+    def eat(self):
+        print(f'{self.name} кушает')
+
+    #Меняем вывод объектов при передаче в функцию print()
+    def __str__(self): return f'{self.name}, {self.age} лет'
+
+#Создаём производные классы животных
+class Bird(Animal):
+    def make_sound(self):
+        print(f'{self.name}: {self.sound}')
+
+class Mammal(Animal):
+    def make_sound(self):
+        print(f'{self.name}: {self.sound}')
+
+class Reptile(Animal):
+    def make_sound(self):
+        print(f'{self.name}: {self.sound}')
+
+#Создаём базовый класс персонала
+class Human:
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+    # Меняем вывод объектов при передаче в функцию print()
+    def __str__(self): return f'{self.name}, {self.position}'
+
+#Создаём производные классы по должностям
+class Veterinar(Human):
+    def heal(self, animal): print(f'{self.position} {self.name} лечит: {animal}')
+
+class Zookeeper(Human):
+    def feed(self, animal): print(f'{self.position} {self.name} кормит: {animal}')
+
+#Создаём класс зоопарка
+class Zoo:
+    def __init__(self):
+        self.animals = []
+        self.staff = []
+
+    def add_animal(self, *args):
+        for animal in args:
+            self.animals.append(animal)
+            print(f'Теперь в нашем зоопарке обитает: {animal}')
+
+    def add_staff(self, *args):
+        for staff in args:
+            self.staff.append(staff)
+            print(f'Теперь в нашем зоопарке работает: {staff}')
+
+    def all_animals(self):
+        if self.animals:
+            print('Животные нашего зоопарка:')
+            for animal in self.animals: print(animal)
+
+    def all_staff(self):
+        if self.staff:
+            print('Персонал нашего зоопарка:')
+            for employee in self.staff: print(employee)
+
+    def animal_sound(self):
+        for animal in self.animals:
+            animal.make_sound()
+
+    def save(self):
+        with open('Animals.txt', 'a', encoding='utf-8') as file:
+            for animal in self.animals:
+                file.write(f'{animal.__class__.__name__}("{animal.name}", "{animal.sound}", {animal.age})' + '\n')
+
+        with open('Staff.txt', 'a', encoding='utf-8') as file:
+            for employee in self.staff:
+                file.write(f'{employee.__class__.__name__}("{employee.name}", "{employee.position}")' + '\n')
+
+    def restore(self):
+        with open('Animals.txt', encoding='utf-8') as file:
+            for line in file.readlines():
+                self.animals.append(eval(line.rstrip('\n')))
+
+        with open('Staff.txt', encoding='utf-8') as file:
+            for line in file.readlines():
+                self.staff.append(eval(line.rstrip('\n')))
